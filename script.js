1d2ab117d2ecfd54583af1b23cdc42d8df51ac92
Lizzie Piazza <epiazza22@gmail.com>
2015-08-25 19:13:51 -0400

    Got clock and switch working

--- script.js
+++ script.js
@@ -2,31 +2,59 @@ var switchOnOff = 'off';
 
 // Get necessary DOM elements
 var timer = document.getElementById('timer');
-var workTime = parseInt(document.getElementById('value-work').childNodes[3].innerHTML);
-var breakTime = parseInt(document.getElementById('value-break').childNodes[3].innerHTML);
-
-var seconds = workTime * 60;
+var workTime = function() {
+	return parseInt(document.getElementById('value-work').childNodes[3].innerHTML);
+}
+var breakTime = function() {
+	return parseInt(document.getElementById('value-break').childNodes[3].innerHTML);
+}
 
 // Functions to set, start, and stop timer
+var alarmSound = new Audio('http://www.freespecialeffects.co.uk/soundfx/sirens/schoolbe.wav');
+
+var seconds;
 
 var setTimer = function(minutes) {
 	seconds = minutes * 60;
-	timer.innerHTML = ('0' + Math.floor(seconds / 60)).slice(-2) + ':' + ('0' + (seconds % 60)).slice(-2); 
+	timer.innerHTML = ('0' + Math.floor(seconds / 60)).slice(-2) + ':' + ('0' + (seconds % 60)).slice(-2);
+	//tartTimer(); 
 }
 
-setTimer(breakTime);
+var timerInterval;
 
 var startTimer = function(){
-	setInterval(function(){
+	timerInterval = setInterval(function(){
 		seconds--;
 		timer.innerHTML = ('0' + Math.floor(seconds / 60)).slice(-2) + ':' + ('0' + (seconds % 60)).slice(-2);
+		// Handle end time
+		if (seconds <= 0){
+			alarmSound.play();
+			if(current == 'work'){
+				current = 'break';
+				setTimer(breakTime());
+			} else {
+				current = 'work';
+				setTimer(workTime());
+			}
+		}
 	}, 1000);
 };
 
-//startTimer();
+var stopTimer = function(){
+	clearInterval(timerInterval);
+}
+
+timer.onclick = function(){
+	if (switchOnOff == 'off'){
+		startTimer();
+		switchOnOff = 'on';
+	} else {
+		stopTimer();
+		switchOnOff = 'off';
+	}
+};
 
 // Make the break and work time adjustable
-// Add an event listener to each button
 var buttons = document.getElementsByClassName("btn");
 
 [].forEach.call(buttons, function (btn) {
@@ -37,9 +65,21 @@ var buttons = document.getElementsByClassName("btn");
 			// Add or subtract
 			if (btn.classList.contains('value-add')) {
 				btn.parentNode.childNodes[3].innerHTML = currentValue + 1;
+				var workTime = parseInt(document.getElementById('value-work').childNodes[3].innerHTML);
+				var breakTime = parseInt(document.getElementById('value-break').childNodes[3].innerHTML);
+				setTimer(workTime);
 			} else if (btn.classList.contains('value-minus')) {
 				btn.parentNode.childNodes[3].innerHTML = currentValue - 1;
+				var workTime = parseInt(document.getElementById('value-work').childNodes[3].innerHTML);
+				var breakTime = parseInt(document.getElementById('value-break').childNodes[3].innerHTML);
+				setTimer(workTime);
 			}
 		}
 	}
 });
+
+// Set the timer
+
+var current = 'work';
+
+setTimer(workTime());
